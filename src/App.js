34c900232dced11c96397a0049c74e587a7d60e8
roboblox <supeecyborg@gmail.com>
2020-06-19 13:30:03 -0400

    not finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Form from "./Form";
 
 const App = () => {
   return (
     <>
+      <header>
+        <nav>
+          <a href="#home"> Home</a>
+          <a href="#about"> About</a>
+          <a href="#content"> Content</a>
+          <a href="#projects"> Projects</a>
+        </nav>
+      </header>
       <h1>Lambda Eats!</h1>
       <p>You can remove this code and create your own header</p>
     </>
